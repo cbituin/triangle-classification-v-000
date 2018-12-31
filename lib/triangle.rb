2de13b23d9aa335d292ec01e59a310aca3704424
Conrad Bituin <inbetweenwebdesigns@gmail.com>
2018-12-31 17:03:23 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -32,8 +32,8 @@ class Triangle
   #violatesInequality tests if triangle violates triangle inequality principle
   def violatesInequality(array)
     array[0] + array[1] < array[2] ||
-    array[0] + array[1] < array[2] ||
-    array[0] + array[1] < array[2]
+    array[2] + array[1] < array[0] ||
+    array[0] + array[2] < array[1]
   end
     
   #countsValid confirms number of equal sides
